67418f91ebcdfde23c382ccfb96117656bc61da2
JarradBaker <jarradbaker@outlook.com>
2023-06-12 13:56:25 +0000

    Add: querys to psycopg and created expression

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -6,8 +6,30 @@ connection = psycopg2.connect(database="chinook")
 # build a cursor object of the database
 cursor = connection.cursor()
 
-# query 1 - select all records from the "Artists" table
-cursor.execute('SELECT * FROM "Artist"')
+# query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Album"')
+
+# query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# query 4 - select only the "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# query 5 - select only the albums with "ArtistId" #51 from the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "AlbumId" = %s', [51])
+
+# query 6 - select all tracks from "Track" table where the composer is "Queen"
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# query 7 - query the database using a different (existing) composer
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Sykes"])
+
+# query 8 - query the database using "test" as the composer
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["test"])
+
 
 # fetch the results (multiple)
 results = cursor.fetchall()
